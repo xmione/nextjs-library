9ec002ff8b1cca46cd8907f6b3856c1430e9ecf6
Solomio Sisante <solomiosisante@gmail.com>
2024-11-30 23:58:59 -0800

    Solved Problem 2: A React Element from an older version of Reac was rendered.

--- go.ps1
+++ go.ps1
@@ -8,26 +8,50 @@ $path = ".\node_modules"
 
 # Start a loop to check if the folder exists
 while (Test-Path $path) {
-    Write-Host "node_modules still exists. Waiting 3 seconds..." -ForegroundColor Yellow
+    Write-Host "$path still exists. Waiting 3 seconds..." -ForegroundColor Yellow
     Start-Sleep -Seconds 3
+
+    Write-Output "rimraf node_modules"
+    rimraf node_modules 
 }
 
-# Define the path to the node_modules folder
+# Define the path to the pnpm-lock.yaml file
 $path = ".\pnpm-lock.yaml"
 
 # Start a loop to check if the folder exists
 while (Test-Path $path) {
-    Write-Host "node_modules still exists. Waiting 3 seconds..." -ForegroundColor Yellow
+    Write-Host "$path still exists. Waiting 3 seconds..." -ForegroundColor Yellow
     Start-Sleep -Seconds 3
+
+    Write-Output "rimraf pnpm-lock.yaml"
+    rimraf pnpm-lock.yaml 
+
 }
 
-# Define the path to the node_modules folder
+# Define the path to the dist folder
+$path = ".\dist"
+
+# Start a loop to check if the folder exists
+while (Test-Path $path) {
+    Write-Host "$path still exists. Waiting 3 seconds..." -ForegroundColor Yellow
+    Start-Sleep -Seconds 3
+
+    Write-Output "rimraf dist"
+    rimraf dist
+
+}
+
+# Define the path to the nextjs-library-1.0.0.tgz file
 $path = ".\nextjs-library-1.0.0.tgz"
 
 # Start a loop to check if the folder exists
 while (Test-Path $path) {
-    Write-Host "node_modules still exists. Waiting 3 seconds..." -ForegroundColor Yellow
+    Write-Host "$path still exists. Waiting 3 seconds..." -ForegroundColor Yellow
     Start-Sleep -Seconds 3
+
+    Write-Output "rimraf nextjs-library-1.0.0.tgz"
+    rimraf nextjs-library-1.0.0.tgz 
+
 }
 
 # Read-Host "Check if they are already deleted. Press Enter key to continue or Ctrl+C to exit."
