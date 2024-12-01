8712ae0ec60f24eee04264dea257463ff0fd5657
Solomio Sisante <solomiosisante@gmail.com>
2024-11-30 21:38:44 -0800

    used ES Module Syntax to fix error: ReferenceError: module is not defined in ES module scope

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-typescript',
-      '@babel/preset-react',
-    ],
-  };
-  
+// babel.config.js
+export default {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+};
